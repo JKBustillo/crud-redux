d4641a13b82a12d6e0abe9303633f4b33b0d2b1f
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-05-29 22:59:03 -0500

    feat: Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-betweent">
             <div className="container">
-                <h1>CRUD - React, Redux, REST & Axios</h1>
+                <h1><Link to="/" className="text-light">CRUD - React, Redux, REST & Axios</Link></h1>
             </div>
 
-            <a  href="/products/new"
+            <Link  to="/products/new"
                 className="btn btn-danger nuevo-post d-block d-md-inline-block"
-            >Add product &#43;</a>
+            >Add product &#43;</Link>
         </nav>
     );
 }
