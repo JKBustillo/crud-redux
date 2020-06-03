4da2ca8ccdc76e2f8dcd91d3a8471b16a690ae5c
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-03 16:05:26 -0500

    feat: useDispatch and deleteProductAction imported and confirmDeleteProduct added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { deleteProductAction } from '../actions/productActions';
 
 const Product = ({ product }) => {
     const { name, price, id } = product;
+
+    const dispatch = useDispatch();
+
+    const confirmDeleteProduct = id => {
+
+
+        dispatch(deleteProductAction(id));
+    }
+
     return (
         <tr>
             <td>{name}</td>
             <td><span className="font-weight-bold">$ {price}</span></td>
             <td className="acciones">
                 <Link to={`/products/${id}`} className="btn btn-primary mr-2">Edit</Link>
-                <button type="button" className="btn btn-danger">Delete</button>
+                <button type="button" className="btn btn-danger" onClick={() => confirmDeleteProduct(id)}>Delete</button>
             </td>
         </tr>
     );
