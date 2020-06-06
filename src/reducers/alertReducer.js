4fe6504e70003040d2f71352d2978c358aa2aee4
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-05 23:40:52 -0500

    feat: new reducer 'alertReducer'

---
+++ src/reducers/alertReducer.js
@@ -0,0 +1,15 @@
+import {
+    SHOW_ALERT,
+    HIDE_ALERT
+} from '../types';
+
+const initialState = {
+    alert: null
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
