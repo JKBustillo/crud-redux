f5d8060b0d3187d84a06decab2333e86f063916f
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-05 23:56:39 -0500

    feat: showAlertAction and showAlert

---
+++ src/actions/alertActions.js
@@ -0,0 +1,15 @@
+import {
+    SHOW_ALERT,
+    HIDE_ALERT
+} from '../types';
+
+export function showAlertAction(alert) {
+    return dispatch => {
+        dispatch(showAlert(alert));
+    }
+}
+
+const showAlert = alert => ({
+    type: SHOW_ALERT,
+    payload: alert
+})
